3ed5cbb4987626fc11788e3dab6b7f71b06173ce
Taylor Gregg Cowley <tcowley0@catnip.ee.byu.edu>
2016-09-12 15:00:57 -0600

    We got our program working, now we just need to do the stupid writeup

--- lab1/Lab1/Lab1/Form1.Designer.cs
+++ lab1/Lab1/Lab1/Form1.Designer.cs
@@ -48,11 +48,11 @@
             // 
             // output
             // 
-            this.output.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.output.Location = new System.Drawing.Point(149, 14);
+            this.output.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(117, 23);
             this.output.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.output.Name = "output";
-            this.output.Size = new System.Drawing.Size(157, 37);
+            this.output.Size = new System.Drawing.Size(275, 26);
             this.output.TabIndex = 1;
             this.output.Text = "impress output";
             this.output.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
--- lab1/Lab1/Lab1/Form1.cs
+++ lab1/Lab1/Lab1/Form1.cs
@@ -29,12 +29,58 @@ namespace Lab1
 
         private void solve_Click(object sender, EventArgs e)
         {
+            // Load the input, apply our algorithm, and give the output. Simple enough.
             output.Text = input.Text;
+            long possible_prime = (Convert.ToInt64(input.Text));
+            if (prime_maybe(possible_prime)) {
+                double k = Convert.ToDouble(k_value.Text);
+                output.Text = "prime! Probablility " + 100.0*(1.0/(Math.Pow(2.0, k))) + "%";
+            } else {
+                output.Text = "Nope, not prime.";
+            }
         }
 
         private void is_prime_Load(object sender, EventArgs e)
         {
 
+        }
+
+        // O(k * log(n) * xy)
+        private bool prime_maybe(long n) {
+            // pick k positive integers at random that are between 2 and k
+            Random rnd = new Random();
+            int test = rnd.Next(2, 12);
+            int k = (Convert.ToInt32(k_value.Text));
+            int[] rands = new int[k];
+            for (int i = 0; i< k; i++){
+                rands[i] = rnd.Next(2, (int) n - 1);
+            }
+
+            // if(a^(n-1) mod n == 1 for all above numbers)
+            foreach (long r in rands) {
+                long exp = mod_exp((int) r, ((int) n - 1), (int) n);
+                if (exp != 1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Produces x^y mod n
+        // O(xy)
+        private long mod_exp(long x, long y, long n) {
+            if(y == 0)
+                return 1;
+            long z = mod_exp(x, y / 2, n);
+            if ((y & 1) == 1) {// means y is odd
+                // return x * z^2 mod n
+                long returnValue = x * ((z * z) % n) % n;
+                return returnValue;
+            } else {
+                // return z^2 mod n
+                long returnValue = (z * z) % n;
+                return returnValue;
+            }
         }
     }
 }
